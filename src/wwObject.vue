3ef950505dbc824fef35aa3f5b7184bdf84d7562
Aurélie VIOLETTE <aurelie@weweb.io>
2020-12-15 13:56:38 +0100

    Init element

---
+++ src/wwObject.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="navigation-menu"></div>
+</template>
+
+<script>
+export default {
+    name: '__COMPONENT_NAME__',
+    wwDefaultContent: {},
+    props: {
+        content: Object,
+        wwFrontState,
+        /* wwManager: start */
+        wwEditorState: Object,
+        /* wwManager: end */
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.navigation-menu {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
